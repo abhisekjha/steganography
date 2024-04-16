5826cc71d82ce83a64fe4c34c6aa5846036ce7d6
Abhisek Kumar Jha <abhisekjha2020@gmail.com>
2024-04-15 22:10:54 -0500

    final commit

--- src/routes/files.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/upload', (req, res) => {
-    res.send('File upload endpoint hit');
-});
-
-module.exports = router;
